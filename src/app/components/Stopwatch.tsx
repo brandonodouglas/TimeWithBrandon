a4305d10370131a12a5ed5befea12375441d07cd
brandonodouglas <brandondouglas247@gmail.com>
2026-01-20 00:11:30 +0000

    added stopwatch with start, reset and pause buttons

--- src/app/components/Stopwatch.tsx
+++ src/app/components/Stopwatch.tsx
@@ -9,9 +9,43 @@ type StopWatchProps = {
 };
 
 export default function StopWatch(props: StopWatchProps) {
-    function onPress(event: GestureResponderEvent): void {
-        Alert.alert('Stopwatch ' + {props}.props.activity + ' started.');
+    const [timerID, setTimerID] = useState(0)
+    const [seconds, setSeconds] = useState(0);
+
+
+    function start() {
+        // start the timer and store the intervalID
+        var intervalID = setInterval(function () {setSeconds(seconds => seconds + 1)}, 1000);
+        setTimerID(intervalID);
     }
+
+    function stop() {
+        console.log('Attempting to stop timerID: ' + {timerID});
+        clearInterval(timerID);
+        setSeconds(0);
+        console.log("Timer stopped");
+    }
+
+    function pause() {
+        console.log("Paused pressed and seconds is currently: " + {seconds});
+        clearInterval(timerID);
+        setSeconds(seconds)
+        console.log("timer paused");
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -20,12 +54,17 @@ export default function StopWatch(props: StopWatchProps) {
     return (
         <View style={{ justifyContent: "center", alignItems: "center" }}>
             <Text style={{ textAlign: 'center', fontSize: 25 }}>{props.activity}  StopWatch</Text>
-            <Text style={{ fontSize: 20, textAlign: 'center' }}>0 seconds</Text>
-            <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 80}} onPress={onPress}>
+            <Text style={{ fontSize: 20, textAlign: 'center' }}>{seconds} seconds</Text>
+            <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 80 }} onPress={start}>
                 <Text style={{ color: 'black', textAlign: 'center', fontSize: 70 }}>▶︎</Text>
             </TouchableOpacity>
-
-
+            <TouchableOpacity style={{ backgroundColor: 'orange', width: 80, height: 80 }} onPress={stop}>
+                <Text style={{ color: 'black', textAlign: 'center', fontSize: 30 }}>STOP</Text>
+            </TouchableOpacity>
+             <TouchableOpacity style={{ backgroundColor: 'yellow', width: 80, height: 80 }} onPress={pause}>
+                <Text style={{ color: 'black', textAlign: 'center', fontSize: 30 }}>PAUSE</Text>
+            </TouchableOpacity>
+           
 
 
         </View>
