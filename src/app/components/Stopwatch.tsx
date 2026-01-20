0ef4ce6c380465c8203e356433b7bb95b9df5d37
brandonodouglas <brandondouglas247@gmail.com>
2026-01-20 15:37:30 +0000

    added all correct stop-watch features:

--- src/app/components/Stopwatch.tsx
+++ src/app/components/Stopwatch.tsx
@@ -8,65 +8,128 @@ type StopWatchProps = {
     activity: string;
 };
 
+// The stopwatch itself which follows - https://www.online-stopwatch.com/
 export default function StopWatch(props: StopWatchProps) {
     const [timerID, setTimerID] = useState(0)
     const [seconds, setSeconds] = useState(0);
+    // State management for the stopwatch
+    const [isPlaying, setIsPlaying] = useState(false);
+    // STATES: INITIAL, RUNNING, CLEARED, PAUSED,
+    const [buttonState, setButtonState] = useState('INITIAL');
+
 
 
     function start() {
         // start the timer and store the intervalID
-        var intervalID = setInterval(function () {setSeconds(seconds => seconds + 1)}, 1000);
+        var intervalID = setInterval(function () { setSeconds(seconds => seconds + 1) }, 1000);
         setTimerID(intervalID);
+        setButtonState('RUNNING');
+
     }
 
-    function stop() {
-        console.log('Attempting to stop timerID: ' + {timerID});
+    function clear() {
+        console.log('Attempting to stop timerID: ' + { timerID });
         clearInterval(timerID);
         setSeconds(0);
+        setButtonState('CLEARED');
+
         console.log("Timer stopped");
     }
 
+
+
+
     function pause() {
-        console.log("Paused pressed and seconds is currently: " + {seconds});
+        console.log("Paused pressed and seconds is currently: " + { seconds });
         clearInterval(timerID);
         setSeconds(seconds)
+        setButtonState('PAUSED')
         console.log("timer paused");
-        
-        
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return (
-        <View style={{ justifyContent: "center", alignItems: "center" }}>
+    if (buttonState == "INITIAL") {
+        return (
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <Text style={{ textAlign: 'center', fontSize: 25 }}>{props.activity}</Text>
+                <Text style={{ fontSize: 20, textAlign: 'center' }}>{seconds} </Text>
+                <Text>helllloooo</Text>
+                <TouchableOpacity style={{ backgroundColor: 'lightgreen', width: 80, height: 80 }} onPress={start}>
+                    <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>tart</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 80 }} onPress={clear}>
+                    <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>Clear</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+    if (buttonState == 'RUNNING') {
+        return (<View style={{ justifyContent: "center", alignItems: "center" }}>
             <Text style={{ textAlign: 'center', fontSize: 25 }}>{props.activity}  StopWatch</Text>
             <Text style={{ fontSize: 20, textAlign: 'center' }}>{seconds} seconds</Text>
-            <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 80 }} onPress={start}>
-                <Text style={{ color: 'black', textAlign: 'center', fontSize: 70 }}>▶︎</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{ backgroundColor: 'orange', width: 80, height: 80 }} onPress={stop}>
-                <Text style={{ color: 'black', textAlign: 'center', fontSize: 30 }}>STOP</Text>
-            </TouchableOpacity>
-             <TouchableOpacity style={{ backgroundColor: 'yellow', width: 80, height: 80 }} onPress={pause}>
-                <Text style={{ color: 'black', textAlign: 'center', fontSize: 30 }}>PAUSE</Text>
-            </TouchableOpacity>
-           
 
+            <TouchableOpacity style={{ backgroundColor: 'lightgreen', width: 80, height: 80 }} onPress={pause}>
+                <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>Pause</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 80 }} onPress={clear}>
+                <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>Clear</Text>
+            </TouchableOpacity>
+
+        </View>)
+    }
+
+    if (buttonState == 'PAUSED') {
+        return (<View style={{ justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ textAlign: 'center', fontSize: 25 }}>{props.activity}  StopWatch</Text>
+            <Text style={{ fontSize: 20, textAlign: 'center' }}>{seconds} seconds</Text>
+
+            <TouchableOpacity style={{ backgroundColor: 'lightgreen', width: 80, height: 80 }} onPress={start}>
+                <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>Continue</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 80 }} onPress={clear}>
+                <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>Clear</Text>
+            </TouchableOpacity>
 
         </View>
-    );
+
+
+        )
+    }
+
+    if (buttonState == "CLEARED") {
+        return (<View style={{ justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ textAlign: 'center', fontSize: 25 }}>{props.activity}  StopWatch</Text>
+            <Text style={{ fontSize: 20, textAlign: 'center' }}>{seconds} seconds</Text>
+
+            <TouchableOpacity style={{ backgroundColor: 'lightgreen', width: 80, height: 80 }} onPress={start}>
+                <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>Start</Text>
+            </TouchableOpacity>
+
+
+
+            <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 80 }} onPress={clear}>
+                <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>Clear</Text>
+            </TouchableOpacity>
+
+        </View>)
+
+    }
+      return (
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <Text style={{ textAlign: 'center', fontSize: 25 }}>{props.activity}</Text>
+                <Text style={{ fontSize: 20, textAlign: 'center' }}>{seconds} </Text>
+                <TouchableOpacity style={{ backgroundColor: 'lightgreen', width: 80, height: 80 }} onPress={start}>
+                    <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>tart</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 80 }} onPress={clear}>
+                    <Text style={{ color: 'black', textAlign: 'center', fontSize: 20 }}>Clear</Text>
+                </TouchableOpacity>
+            </View>
+        )
+
+
+
+
 }
