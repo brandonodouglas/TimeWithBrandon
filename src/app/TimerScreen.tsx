69e745ef3d6334750d3c02007add774d0c007baa
brandonodouglas <brandondouglas247@gmail.com>
2026-01-27 18:26:04 +0000

    Cleaned up whitespace

--- src/app/TimerScreen.tsx
+++ src/app/TimerScreen.tsx
@@ -3,23 +3,16 @@ import React, { useState } from 'react';
 import StopWatch from './components/Stopwatch';
 import StopWatchLabel from './components/StopwatchLabel';
 import uuid from 'react-native-uuid';
-
-
-
 type StopWatchData = {
   id: string;
   label: string;
 }
-
 const DATA: StopWatchData[] = [];
-
-
 // Screen that opens when the user wants to start a timer
 export default function TimerScreen() {
   const [idVal, setIdVal] = useState(0);
   const [selectedId, setSelectedId] = useState<string>();
-
-  const [stopWatchLabels, setStopWatchLabels] = useState([{id: uuid.v4(), label:'example1'}]); // The name/text of the stopwatches
+  const [stopWatchLabels, setStopWatchLabels] = useState([{ id: uuid.v4(), label: 'example1' }]); // The name/text of the stopwatches
   const [data, setData] = useState([]);
   // Counter which is an interator for the stopwatches
   const [stopWatchDelete, setStopWatchDelete] = useState(false);
@@ -28,53 +21,33 @@ export default function TimerScreen() {
   }
   function removeItem(idToDelete: string) {
     setStopWatchLabels(stopWatchLabels.filter((listValue) => listValue.id !== idToDelete));
-
   }
-
-  
- 
-
- 
-
   return (
     <View style={{ justifyContent: 'center', padding: 20 }}>
       <Text style={{ fontSize: 30, textAlign: 'center' }}>My Timers</Text>
       <Text style={{ fontSize: 25, textAlign: 'center' }}>You currently have {stopWatchLabels.length} timers.</Text>
       <Text style={{ fontSize: 15, textAlign: 'center' }}>Tap on the timer name to edit the time name.</Text>
       <TextInput style={styles.input} placeholder='Enter timer name here' onSubmitEditing={event => {
-
-        setStopWatchLabels([...stopWatchLabels, {id:uuid.v4(),label:event.nativeEvent.text} ]);
-        setIdVal(idVal+1);
-
+        setStopWatchLabels([...stopWatchLabels, { id: uuid.v4(), label: event.nativeEvent.text }]);
+        setIdVal(idVal + 1);
       }} />
-
-
       {/* He we iterate through the string list and create a array of stopwatch timer objects */}
       <FlatList
         data={stopWatchLabels}
-
-        renderItem={({ item, index}) => (<View style={{
+        renderItem={({ item, index }) => (<View style={{
           alignItems: 'center',
           flex: 1,
           justifyContent: 'center'
         }}><StopWatch stopWatchName={item.label} deleted={stopWatchDelete} />
           <TouchableOpacity style={{ backgroundColor: 'red', width: 80, height: 30 }} onPress={() => {
-    removeItem(item.id);
-  }}>
+            removeItem(item.id);
+          }}>
             <Text style={{ color: 'black', textAlign: 'center', fontSize: 20, width: 80, height: 30 }}>Delete</Text>
           </TouchableOpacity>
         </View>
         )}
-
-
-
         keyExtractor={(item) => item.id}
       />
-
-
-
-
-
     </View>
   );
 }
