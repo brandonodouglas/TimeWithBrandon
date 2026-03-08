80514f84bd9177ec5c4440e4c53b94fed4650f9e
brandonodouglas <brandondouglas247@gmail.com>
2026-03-08 14:14:49 +0000

    set up the code before useEffect hook, to make the data persist when the user navigates back and to the database folder page

--- src/app/FolderScreen.tsx
+++ src/app/FolderScreen.tsx
@@ -31,7 +31,7 @@ export default function FolderScreen() {
   // Store folder data within the database itself
   // If just landing
   const [isStart, setIsStart] = useState(true)
-  
+
 
 
 
@@ -41,8 +41,8 @@ export default function FolderScreen() {
     for (const key of folderKeys) {
       let arrayKey = key // key
       let arrayValue = await AsyncStorage.getItem(arrayKey) // value
-      setGlobalStoredFolders([...stopWatchLabels, { id: arrayKey, label: arrayValue}]);
-    
+      setGlobalStoredFolders([...stopWatchLabels, { id: arrayKey, label: arrayValue }]);
+
     }
 
     return folderKeys;
@@ -53,13 +53,13 @@ export default function FolderScreen() {
     return folderCount;
   }
 
-  
+
   // Clear the folder database
   const clearFolderDatabase = async () => {
     await AsyncStorage.clear();
   }
 
-  
+
 
 
 
@@ -111,17 +111,17 @@ export default function FolderScreen() {
     } else {
       for (const values of formattedArray) {
         const key = await AsyncStorage.getItem(values)
-        let folderObject = {id: values, label: key}
+        let folderObject = { id: values, label: key }
         folderArray.push(folderObject)
-       
+
 
 
       }
 
     }
     return folderArray;
-  
-    
+
+
 
   }
 
@@ -141,14 +141,14 @@ export default function FolderScreen() {
 
 
 
-    // Deletes folder item from the database AND the stopwatch list given unique id string
+  // Deletes folder item from the database AND the stopwatch list given unique id string
 
   async function removeItem(idToDelete: string) {
     setStopWatchLabels(stopWatchLabels.filter((listValue) => listValue.id !== idToDelete))
     setGlobalStoredFolders(stopWatchLabels.filter((listValue) => listValue.id !== idToDelete))
     // Remove from local database
     await AsyncStorage.removeItem(idToDelete)
-  
+
 
 
 
@@ -166,18 +166,18 @@ export default function FolderScreen() {
 
 
   }
-// there's folders found within the database, display them basically.
+  // there's folders found within the database, display them basically.
 
 
 
-    console.log('[DATABASE]: Beginning storage.') // Used for the developer
-    // get the folders from the database
-    
+  console.log('[DATABASE]: Beginning storage.') // Used for the developer
+  // get the folders from the database
 
-    // if there's folders in the datbase
-    if (globalStoredFolders.length != 0) {
-      console.log(globalStoredFolders.length)
-      // If the user hasn't clicked on a folder to change the folder name itself
+
+  // if there's folders in the datbase
+  if (globalStoredFolders.length != 0) {
+    console.log('There are currently: ' + globalStoredFolders.length + ' folders in the database, loading them now')
+    // If the user hasn't clicked on a folder to change the folder name itself
     if (!editing) {
       return (<View style={{ justifyContent: 'center', padding: 20 }}>
         <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Capture your folders here.</Text>
@@ -190,7 +190,7 @@ export default function FolderScreen() {
 
 
         <FlatList
-          data={stopWatchLabels}
+          data={globalStoredFolders}
 
           renderItem={({ item }) => (<View style={{
             alignItems: 'center',
@@ -246,17 +246,10 @@ export default function FolderScreen() {
 
             <TextInput style={styles.input} placeholder='Enter new folder name' onSubmitEditing={event => {
               setEditing(false)
-              for (let i = 0; i < stopWatchLabels.length; i++) {
 
-
-              }
 
               setStopWatchLabels(stopWatchLabels.map(element => ({ id: element.id, label: event.nativeEvent.text })))
 
-              for (let i = 0; i < stopWatchLabels.length; i++) {
-
-
-              }
             }} />
             <Button onPress={() => editFolderName(item.label, item.id)} title="Edit folder name"></Button>
             <Button onPress={() => newScreen(item.label, item.id)} title="new screen"></Button>
@@ -275,29 +268,124 @@ export default function FolderScreen() {
         />
 
 
-  
+
 
       </View>
 
 
       )
-    } 
-    
-
-    
-    }
-    // When there's folders in the database
-    else {
-      console.log("hello")
-      return (<View><Text>You currently have no folers in the database! Enter some below now!</Text></View>)
-      // Here we 
     }
 
 
 
+  }
+  // When there's folders in the database
+  else {
+    console.log(globalStoredFolders)
+     if (!editing) {
+      return (<View style={{ justifyContent: 'center', padding: 20 }}>
+        <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>There are currently no folders in your database.</Text>
+        <TextInput style={styles.input} placeholder='Enter folder name here' onSubmitEditing={event => {
+          let myID = uuid.v4()
+          setStopWatchLabels([...stopWatchLabels, { id: myID, label: event.nativeEvent.text }]);
+          setGlobalStoredFolders(stopWatchLabels)
+          addFolderData(event.nativeEvent.text, myID) // this add data into the database
+        }} />
 
 
-  
+        <FlatList
+          data={globalStoredFolders}
+
+          renderItem={({ item }) => (<View style={{
+            alignItems: 'center',
+            flex: 1,
+            justifyContent: 'center'
+          }}>
+
+            <Text style={{ color: 'black', textAlign: 'center', fontSize: 0, fontWeight: "800" }}>{"📁" + item.label}</Text>
+            <Button onPress={() => { setEditing(true) }} title="Edit folder name"></Button>
+            <Button onPress={() => newScreen(item.label, item.id)} title="new screen"></Button>
+            <Button onPress={() => removeItem(item.id)} title="Delete"></Button>
+            <Button onPress={fetchFolderData} title="Database"></Button>
+
+
+
+
+
+
+
+
+
+
+          </View>
+
+          )}
+
+
+          keyExtractor={(item) => item.id}
+
+        />
+        <Button onPress={() => clearAllFolders()} title="Delete and clear all folders"></Button>
+
+
+      </View>
+
+
+      )
+
+    } else {
+      // If the user has clicked a folder then a textbox appears to change the name
+      return (<View style={{ justifyContent: 'center', padding: 20 }}>
+        <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>Capture your folders here.</Text>
+        <TextInput style={styles.input} placeholder='Enter folder name here' onSubmitEditing={event => {
+          setStopWatchLabels([...stopWatchLabels, { id: uuid.v4(), label: event.nativeEvent.text }]);
+        }} />
+        <FlatList
+          data={stopWatchLabels}
+          renderItem={({ item }) => (<View style={{
+            alignItems: 'center',
+            flex: 1,
+            justifyContent: 'center'
+          }}>
+
+            <TextInput style={styles.input} placeholder='Enter new folder name' onSubmitEditing={event => {
+              setEditing(false)
+
+
+              setStopWatchLabels(stopWatchLabels.map(element => ({ id: element.id, label: event.nativeEvent.text })))
+
+            }} />
+            <Button onPress={() => editFolderName(item.label, item.id)} title="Edit folder name"></Button>
+            <Button onPress={() => newScreen(item.label, item.id)} title="new screen"></Button>
+            <Button onPress={() => removeItem(item.id)} title="Delete"></Button>
+
+
+
+
+          </View>
+
+          )}
+
+
+          keyExtractor={(item) => item.id}
+
+        />
+
+
+
+
+      </View>
+
+
+      )
+    }
+  }
+
+
+
+
+
+
 
 
 
